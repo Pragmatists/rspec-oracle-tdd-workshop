5660e2582186d3607f59efa790c76930dff844e3
Jacek Gębal <jgebal@gmail.com>
2015-04-30 23:13:58 +0200

    Updated failing Rspec tests. Extended reference data to support feature.

--- spec/table_data_processing/raise_salaries_spec.rb
+++ spec/table_data_processing/raise_salaries_spec.rb
@@ -34,30 +34,6 @@ describe 'Raise Employees salaries' do
 
   end
 
-  it 'raise salary by percent only' do
-    employee_row = {
-        :salary => 200,
-        :employee_id => 1
-    }
-    max_salary = 500
-    percent = 50
-
-    updated_employee_row = plsql.hr.emp.calculate_salary(employee_row, max_salary, percent)
-    expect(updated_employee_row[:salary]).to eq (300)
-    expect(updated_employee_row[:employee_id]).to eq (1)
-  end
-
-  it 'salary cant exceed max' do
-    employee_row = {
-        :salary => 200
-    }
-    max_salary = 210
-    percent = 50
-
-    updated_employee_row = plsql.hr.emp.calculate_salary(employee_row, max_salary, percent)
-    expect(updated_employee_row[:salary]).to eq (210)
-  end
-
   def refresh(employee)
     plsql.hr.employees.first(employee_id: employee[:employee_id])
   end
